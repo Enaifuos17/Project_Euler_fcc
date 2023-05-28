240dad03aa633b4c7f109946208c4faf440973a8
Enaifuos17 <soufianeelfaquir2001@gmail.com>
2023-05-28 15:10:09 +0100

    P9 - still trying to understand the P9

---
+++ 001_100/p9.js
@@ -0,0 +1,29 @@
+/*
+
+  * P8 - Special Pythagorean triplet
+
+  A Pythagorean triplet is a set of three natural numbers: 
+  a < b < c, for which:
+  a^2 + b^2 = c^2
+
+  for example:
+  3^2 + 4^2 = 9 + 16 = 25 = 5^2
+
+  There exists exactly one Pythagorean triplet for which
+  a + b + c = 1000. 
+  Find the product abc such that a + b + c = n.
+
+*/
+
+function specialPythagoreanTriplet(n) {
+  let sumOfabc = n;
+
+  return sumOfabc;
+}
+
+console.log(specialPythagoreanTriplet(24)); // 480
+// console.log(specialPythagoreanTriplet(120)); // 49920, 55080, 60000
+// console.log(specialPythagoreanTriplet(1000)); // 31875000
+
+// test area
+console.log("*".repeat(20));
